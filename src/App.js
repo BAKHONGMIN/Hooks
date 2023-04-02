d1703e40c4a3e7c29a95c108e35a598058894c01
BAKHONGMIN <ghdals2655@naver.com>
2023-04-03 00:45:49 +0900

    revert
    revert

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import { Component } from "react";
-import SassComponent from "./SassComponent";
+import Average from "./Average";
 
-class App extends Component{
-  render(){
-    return(
-      <div>
-        <SassComponent/>
-      </div>
-    )
-  }
+const App = () => {
+  return <Average />;
 };
 
 export default App;
